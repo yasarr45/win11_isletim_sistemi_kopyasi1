15d80dd316a65b0126febad3925302e31f95f8ed
Yasar Bilgi <ybilgi92@gmail.com>
2026-05-03 15:15:05 +0300

    base ayarı eklendi

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/win11_isletim_sistemi_kopyasi1/", // 👈 BUNU EKLEDİK
   plugins: [react()],
 })
